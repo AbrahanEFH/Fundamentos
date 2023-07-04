15a8a0ab91e05d8c57ce101fcffb0f94ea664ac6
abrahan <abrahandev@gmail.com>
2023-07-04 09:25:30 +0200

    Copiando objetos

--- js/script05.js
+++ js/script05.js
@@ -1,1 +1,22 @@
 // union de dos o mas objetos
+
+const producto = {
+    nombre: 'Tv',
+    precio: 679,
+    disponible: true
+}
+
+const cliente = {
+    nombre: 'Pancho',
+    premium: false
+}
+
+//const nuevoObjeto = { ...producto, ...cliente }
+
+// Otra forma
+const nuevoObjeto = {
+    producto: { ...producto },
+    cliente: { ...cliente}
+}
+
+console.log(nuevoObjeto)
