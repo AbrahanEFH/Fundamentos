ab09bd5613ec580ef6c0fcde15eda84119981909
abrahan <abrahandev@gmail.com>
2023-07-04 22:16:45 +0200

    Funciones

--- js/script07.js
+++ js/script07.js
@@ -1,3 +1,19 @@
 // Destructuring de arrays
 
 const tecnologies = ['Html', 'Css', 'Node.js', 'PHP', 'React']
+
+// const [  html ] = tecnologies
+
+// console.log(html)
+
+// Iterar en arreglos
+// forEach  Acceder a cada elemento del array
+// tecnologies.forEach(function (tec) {
+//     console.log(tec)
+// })
+
+const arrayMap = tecnologies.map(tec => {
+    return tec
+});
+
+console.log(arrayMap)
