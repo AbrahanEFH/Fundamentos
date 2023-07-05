044a9a79026091753297838ba9dbdb15d394ff82
abrahan <abrahandev@gmail.com>
2023-07-05 18:03:03 +0200

    Arrow functions

---
+++ js/script08.js
@@ -0,0 +1,19 @@
+// Funcion - Function Declaration
+
+
+
+// function sumar(num1, num2){
+//     return num1 + num2
+// }
+
+// const resultado = sumar( 2, 40)
+
+// console.log(resultado)
+
+// Function Expression
+
+const sumar = function(){
+    console.log(2+2)
+}
+
+sumar()
