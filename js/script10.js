8ca4ab773a87cf4586dcde17a68eef41a377e763
abrahan <abrahandev@gmail.com>
2023-07-05 19:34:09 +0200

    Operador ternario

--- js/script10.js
+++ js/script10.js
@@ -1,1 +1,17 @@
-//
+// 
+
+// const saldo = 500
+// const pagar = 800
+// const card = false
+
+// if(saldo >= pagar || card){
+//     alert(' Gracias por la compra')
+// } else {
+//     alert('Saldo insuficiente')
+// }
+
+// Operador ternario
+
+const autenticado = false
+
+autenticado ? alert('Usuario autenticado') : alert('Debe autenticarse')
