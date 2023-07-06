b3786929e6aa048c880a06da5857e0580dc8291f
abrahan <abrahandev@gmail.com>
2023-07-06 08:51:03 +0200

    Manipulacion del Dom

--- js/script11.js
+++ js/script11.js
@@ -1,3 +1,14 @@
 // Selectores del dom
 
 const heading = document.querySelector('#heading')
+
+heading.textContent = 'Manipulando el HTML'
+
+
+console.log(heading.textContent)
+const inputNombre = document.querySelector('#nombre')
+
+
+
+const enlaces = document.querySelectorAll('.navegacion a')
+enlaces.forEach( enlace => enlace.textContent = 'Nuevo Enlace')
