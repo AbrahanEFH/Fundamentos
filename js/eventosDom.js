0276ca74806664c4a3fd78d058044a32145d3f6a
abrahan <abrahandev@gmail.com>
2023-07-07 09:30:07 +0200

    Ahora el input

--- js/eventosDom.js
+++ js/eventosDom.js
@@ -4,3 +4,13 @@ const heading = document.querySelector('#heading')
 heading.addEventListener('click', () => {
     heading.textContent = 'Nuevo HEading al hacr click'
 })
+
+const enlaces = document.querySelectorAll('.navegacion a')
+
+// para poder manipular el Dom de multiples elementos se necesita iterar sobre cada uno de ellos 
+
+enlaces.forEach(enlace => {
+    enlace.addEventListener('click', () => {
+        enlace.textContent = 'Click en Enlaces'
+    })
+});
