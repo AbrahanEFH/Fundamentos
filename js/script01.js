a42eba3e12fede7b9f4204b1c8ca72cb51a4c424
abrahan <abrahandev@gmail.com>
2023-11-24 17:09:40 +0100

    try catch en async await

--- js/script01.js
+++ js/script01.js
@@ -15,3 +15,33 @@ try {
 } catch (error) {
     console.log(error)
 }
+
+function descargarCliente() {
+
+    return new Promise((resolve, reject) => {
+        const error = false
+
+        setTimeout(() => {
+            if (!error) {
+                resolve('El listado de clientes se descargo correctamente')
+            } else {
+                reject('Error en la conexion')
+            }
+        }, 3000);
+    })
+}
+
+// Async await
+
+async function ejecutar() {
+    try {
+        const respuesta = await descargarCliente()
+
+        console.log(2 + 2)
+        console.log(respuesta)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+ejecutar()
