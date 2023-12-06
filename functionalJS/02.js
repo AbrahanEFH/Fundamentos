81245a8214af13492305a8f6c66903efff4b81d2
abrahan <abrahandev@gmail.com>
2023-12-06 08:29:42 +0100

    Uso de currying and partials

--- functionalJS/02.js
+++ functionalJS/02.js
@@ -23,3 +23,26 @@ const obtenercliente = () => {
 const cliente = obtenercliente()
 
 cliente()
+
+// Currying and PArtials
+
+const suma = (a, b, c) => {
+    return a + b + c
+}
+
+console.log(suma(2, 3, 4))
+
+const parcial = a => (b, c) => suma(a, b, c)
+
+const primerNumero = parcial(5)
+const resultado = primerNumero(4, 3)
+
+console.log(resultado)
+
+const parcial2 = a => b => c => suma(a, b, c)
+
+const numero1 = parcial2(5)
+const numero2 = numero1(4)
+const parcialResultado = numero2(3)
+
+console.log(resultado)
